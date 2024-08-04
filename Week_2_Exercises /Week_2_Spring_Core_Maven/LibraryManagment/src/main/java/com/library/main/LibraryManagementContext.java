3f42d36db803222010fe2798d46a6926be738bcb
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-08-04 15:00:27 +0530

    Add files via upload

---
+++ Week_2_Exercises /Week_2_Spring_Core_Maven/LibraryManagment/src/main/java/com/library/main/LibraryManagementContext.java
@@ -0,0 +1,16 @@
+package com.library.main;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class LibraryManagementContext {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	        BookService bookService = (BookService) context.getBean("bookService");
+	        bookService.addBook();
+	    
+	}
+
+}
