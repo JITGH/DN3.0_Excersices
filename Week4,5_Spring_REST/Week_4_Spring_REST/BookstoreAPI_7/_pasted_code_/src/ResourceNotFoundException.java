c089e95f25544919422178d0d5078a95469c63c9
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-08-22 00:43:21 +0530

    ok

--- Week4,5_Spring_REST/Week_4_Spring_REST/BookstoreAPI_7/_pasted_code_/src/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException() {
-    }
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
