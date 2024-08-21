f8e0b3989e9444a6c485b1e42970791b25596736
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-08-22 00:41:10 +0530

    Add files via upload

---
+++ Week4,5_Spring_REST/Week_4_Spring_REST/BookstoreAPI_7/_pasted_code_/src/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException() {
+    }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
